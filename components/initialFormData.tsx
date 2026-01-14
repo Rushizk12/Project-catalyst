c1bfda25630dc2ff9c41fc6b92e1cd1140bd3860
Rushizk12 <rushikesh7981@gmail.com>
2026-01-15 02:33:53 +0530

    fixing

---
+++ components/initialFormData.tsx
@@ -0,0 +1,13 @@
+import { ProjectFormData } from '@/types';
+
+export const initialFormData: ProjectFormData = {
+  name: '',
+  email: '',
+  phoneNumber: '',
+  collegeName: '',
+  address: '',
+  projectTitle: '',
+  projectDescription: '',
+  projectType: 'web',
+  budget: '5000',
+};
